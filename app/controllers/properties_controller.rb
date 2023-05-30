53043deedded2e9f9aeeeff6fb7b5ddef3449e6b
Jared Champayne <36553884+jrod8253@users.noreply.github.com>
2023-05-30 12:09:28 +0200

    Revert "Properties show"

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,16 +1,2 @@
 class PropertiesController < ApplicationController
-  before_action :set_property, only: :show
-
-  def show
-  end
-
-  private
-
-  def set_property
-    @property = Property.find(params[:id])
-  end
-
-  def property_params
-    params.require(:properties).permit(:name, :address, :description, :price_per_night, :number_of_guests, :user_id)
-  end
 end
