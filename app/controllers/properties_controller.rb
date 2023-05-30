eda66589c1826ec6eaf07d0ae55a91c84ffee12c
Alejandro Rodriguez Hernandez <alejarod01@gmail.com>
2023-05-30 11:48:38 +0200

    Added Methods: new, create. Added user authentication prior new/create

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,21 @@
 class PropertiesController < ApplicationController
+    def new
+        @property = Property.new
+    end
+    
+    def create
+        @property = Property.new(property_params)
+        @property.user = current_user
+
+        if @property.save
+            redirect_to property_path(@property)
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def property_params
+        params.require(:property).permit(:name, :address, :description, :price_per_night, :number_of_guests)        
+    end
 end
