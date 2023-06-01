7c380140623c5fe9e17d4401c4d29431a6ae4731
Parker Budding <parkerbudding@gmail.com>
2023-06-01 17:27:30 +0200

    favorites is setup on dasjbvoard and link

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :properties do
     resources :reservations, only: [ :new, :create ]
+    resources :favorites, only: [ :create ]
   end
   resources :reservations, only: [ :edit, :update, :destroy, :index ]
 
